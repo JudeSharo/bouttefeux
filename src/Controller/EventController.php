c32cbbe810980288a298daca5ffb644d7e6a43ac
JudeSharo <arthurdelhayepro@gmail.com>
2021-05-10 07:55:47 +0200

    Enregistrement termine

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class EventController extends AbstractController
 {
-    #[Route('/', name: 'event')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('event/index.html.twig', [
