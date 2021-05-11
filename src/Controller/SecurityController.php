0f353f5d73449afc96cf35c3fb4129ffea2bc080
JudeSharo <arthurdelhayepro@gmail.com>
2021-05-11 15:24:40 +0200

    Registration fini+ debut du profil

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -23,10 +23,10 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('event/index.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    #[Route('/logout', name: 'logout')]
+    #[Route('/logout', name: 'app_logout')]
     public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
