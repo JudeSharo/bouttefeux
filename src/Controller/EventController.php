c705290250a65a1004b94b33e79fc902630d88ac
JudeSharo <arthurdelhayepro@gmail.com>
2021-05-12 15:47:14 +0200

    Retour en arriere :'( creation de l'event sans l'image

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Form\EventType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,6 +16,27 @@ class EventController extends AbstractController
     {
         return $this->render('event/index.html.twig', [
             'controller_name' => 'EventController',
+        ]);
+    }
+
+    #[Route('/event/new',name:'app_event_new')]
+    public function new(Request $request):Response
+    {
+    	$event = new Event();
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($event);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('event/new.html.twig', [
+            'event' => $event,
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Entity/Event.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EventRepository::class)
+ */
+class Event
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Description;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Image::class, mappedBy="event")
+     */
+    private $Image;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $Datetime;
+
+    public function __construct()
+    {
+        $this->Image = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): self
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Description): self
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Image[]
+     */
+    public function getImage(): Collection
+    {
+        return $this->Image;
+    }
+
+    public function addImage(Image $image): self
+    {
+        if (!$this->Image->contains($image)) {
+            $this->Image[] = $image;
+            $image->setEvent($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImage(Image $image): self
+    {
+        if ($this->Image->removeElement($image)) {
+            // set the owning side to null (unless already changed)
+            if ($image->getEvent() === $this) {
+                $image->setEvent(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getDatetime(): ?\DateTimeInterface
+    {
+        return $this->Datetime;
+    }
+
+    public function setDatetime(\DateTimeInterface $Datetime): self
+    {
+        $this->Datetime = $Datetime;
+
+        return $this;
+    }
+}
