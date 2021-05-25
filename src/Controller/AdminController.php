43ad4f6256a9fb824a280e64b0b2632e2f1239b9
JudeSharo <arthurdelhayepro@gmail.com>
2021-05-25 09:25:47 +0200

    Bug fix table admin/index+ ajout option supprimer un utilisateur

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,7 +14,17 @@ class AdminController extends AbstractController
     public function index(UserRepository $userRepository): Response
     {
         return $this->render('admin/index.html.twig', [
-            'user' => $userRepository->findAll(),
+            'users' => $userRepository->findAll(),
         ]);
+    }
+
+    #[Route('/admin/{id}/delete',name:'app_admin_delete',methods:['POST'])]
+    public function deleteEvent(User $user)
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush();
+        return $this->redirectToRoute('admin/index');
     }
 }
